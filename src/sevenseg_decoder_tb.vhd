5b2884e45a5ba334de9b075416d88affef6b862b
ObAkinyemi <c28obaloluwa.akinyemi@afacademy.af.edu>
2025-03-02 17:08:56 -0700

    I was able to creat e testbench, and I trid running the sim, but idk what's going on

--- src/sevenseg_decoder_tb.vhd
+++ src/sevenseg_decoder_tb.vhd
@@ -32,13 +32,42 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity sevenseg_decoder_tb is
-    Port ( i_Hex : in STD_LOGIC_VECTOR (3 downto 0);
-           o_seg_n : out STD_LOGIC_VECTOR (6 downto 0));
 end sevenseg_decoder_tb;
 
-architecture Behavioral of sevenseg_decoder_tb is
+architecture test_bench of sevenseg_decoder_tb is
+   
+-- declare the component of hte seven_seg_decoder
+    component sevenseg_decoder is
+        Port ( i_Hex : in STD_LOGIC_VECTOR (3 downto 0);
+               o_seg_n : out STD_LOGIC_VECTOR (6 downto 0));
+    end component sevenseg_decoder;
+    
+-- declare the signals
+    signal input    :   std_logic_vector (3 downto 0) :=x"0000";
+    signal out_segs :   std_logic_vector (6 downto 0) :=x"00000000";
 
 begin
 
+-- PORT maps
+    sevenseg_decoder_uut : sevenseg_decoder port map (
+        i_Hex   => input(3 downto 0),
+        o_seg_n => out_segs(6 downto 0)
+    );
+    
+    
+    test_process : process
+   
+    begin
+        input <= x"A"; wait for 10 ns;
+            assert(o_seg_n = x"1110111") report "bad with zeros" severity failure;
+        input <= x"B"; wait for 10 ns;
+            assert(o_seg_n = x"0011111") report "bad with zeros" severity failure;
+        input <= x"5"; wait for 10 ns;
+            assert(o_seg_n = x"1011011") report "bad with zeros" severity failure;
+        input <= x"6"; wait for 10 ns;
+            assert(o_seg_n = x"1011111") report "bad with zeros" severity failure;
+   
+    wait;
+    end process;
 
-end Behavioral;
+end test_bench;
